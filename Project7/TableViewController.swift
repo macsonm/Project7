66413ea552527a9b4e40b0f3734ef6e5d63d797c
macsonm <75022784+macsonm@users.noreply.github.com>
2022-07-17 14:32:14 +0300

    URL,JSONDecoder,WKWebView

---
+++ Project7/TableViewController.swift
@@ -0,0 +1,52 @@
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    var petitions = [Petition]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url) {
+                parse(json: data)
+            }
+        }
+    }
+
+    func parse(json: Data) {
+        let decoder = JSONDecoder()
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+            petitions = jsonPetitions.results
+            tableView.reloadData()
+        }
+    }
+    
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let petition = petitions[indexPath.row]
+        
+        var content = cell.defaultContentConfiguration()
+        content.text = petition.title
+        content.secondaryText = petition.body
+        
+        cell.contentConfiguration = content
+
+        return cell
+    }
+
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.detailItem = petitions[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
+    }
+
+}
